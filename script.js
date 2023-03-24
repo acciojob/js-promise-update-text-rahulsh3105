5fd9b19a27d59bc6ee5a832da506f18fe4a4ca37
Rahul Sharma <rahulsh3105@gmail.com>
2023-03-24 11:56:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-//your JS code here. If required.
 function updateText() {
-  // Create a promise that resolves with the string "Hello, world!" after 1 second
-  const promise = new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("Hello, world!");
     }, 1000);
   });
-
-  // Update the text of the HTML element with ID "output" after the promise resolves
-  promise.then((text) => {
-    $("#output").text(text);
-  });
 }
 
-// Call the function to update the text
-updateText();
+updateText()
+  .then((text) => {
+    document.getElementById("output").textContent = text;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
